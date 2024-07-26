1993d45da27de86b7fdff68be2561f23d2e3eb6d
Ankit Kumar <118598590+iamankit7667@users.noreply.github.com>
2024-07-26 17:06:20 +0530

    Add files via upload

--- client/src/AxiosConfig.js
+++ client/src/AxiosConfig.js
@@ -1,18 +1,7 @@
 import axios from "axios";
 
 const getBaseUrl = () => {
-  let url;
-  switch ("production") {
-    case "production":
-      url = "https://backend-packurbags.onrender.com/";
-      break;
-    case "azure":
-      url = "https://backend-packurbags.azurewebsites.net/";
-      break;
-    case "development":
-    default:
-      url = "http://localhost:9000/";
-  }
+  let url = "http://localhost:9000/";
   return url;
 };
 
